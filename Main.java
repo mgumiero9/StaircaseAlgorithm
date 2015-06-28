a079ec60107287b5805b95a4175ce9585b5ee273
Marcelo Gumiero <mgumiero9@gmail.com>
2015-06-28 20:01:14 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Input //
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        String step="";
+        String space=" ";
+        // Output //
+        for (int i = 0; i < t; i++) {
+            for (int j = t - i -2; j >= 0 ; j--) {
+                System.out.print(space);
+            }
+            step += "#";
+            System.out.print(step);
+            System.out.println();
+        }
+    }
+}
